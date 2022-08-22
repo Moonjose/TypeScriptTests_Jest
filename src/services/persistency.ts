cb0ece9c48e1c5a71fe03fb4ed7e624f739aa37b
Jose Antonio <ajose9975@gmail.com>
2022-08-22 14:30:02 -0300

    Spy On Tests

--- src/services/persistency.ts
+++ src/services/persistency.ts
@@ -2,6 +2,6 @@ import { IPersistency } from '../classes/interfaces/IPersistency';
 
 export class Persistency implements IPersistency {
   saveOrder(): void {
-    console.log('Pedido salvo com sucesso');
+    console.log('Pedido salvo com sucesso...');
   }
 }
